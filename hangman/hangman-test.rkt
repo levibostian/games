1ad8b71e666cf1f49b9308fa8fcd6f1f85a106c1
Levi Bostian <levi.bostian@gmail.com>
2015-02-07 02:17:57 -0600

    Display slots for length of word.

--- hangman/hangman-test.rkt
+++ hangman/hangman-test.rkt
@@ -5,5 +5,11 @@
 (require "hangman.rkt")
 (require rackunit)
 
+;;--------------------------------------
+;; various game backend functions
 (check-equal? (get-number-nouns) 2328)
 (check-equal? (get-noun-at-line-number 1) "abyssinian")
+
+;;------------------------------------
+;; User interface specific functions
+(check-equal? (string-length (get-word-slots 3)) 6)
--- hangman/hangman.rkt
+++ hangman/hangman.rkt
@@ -2,9 +2,13 @@
 
 (provide (all-defined-out))
 
+;;--------------------------------------
+;; constants
 (define nouns-filename "nounlist.txt")
-
 (define nouns-file (open-input-file nouns-filename))
+
+;;--------------------------------------
+;; various game backend functions
 
 (define (get-number-nouns)
   (get-number-nouns-helper (open-input-file nouns-filename) 0))
@@ -26,11 +30,29 @@
   (let ([noun-num-choice (random (get-number-nouns))])
     (get-noun-at-line-number noun-num-choice)))
 
-;;------------------------------------------
+;;-----------------------------------------
+;; User Interface specific functions
 
-(define hangman
-  (lambda ()
-    (printf "Welcome to hangman.")
-    (printf (number->string (get-number-nouns)))))
+(define (print-home-screen)
+  (printf "--------------------------------------------") (newline)
+  (printf "--         Welcome to Hangman             --") (newline)
+  (printf "--------------------------------------------") (newline))
+
+(define (print-word-slots num-slots)
+  (newline)
+  (printf (get-word-slots num-slots))
+  (newline))
+
+(define (get-word-slots num-slots)
+  (letrec ([print-a-slot 
+                    (lambda (num-slots-left accum-list)
+                      (if (eq? 0 num-slots-left)
+                          accum-list
+                          (print-a-slot (- num-slots-left 1) (string-append accum-list "_ "))))])
+            (print-a-slot num-slots "")))
+;;------------------------------------------
+(define (hangman)
+  (print-home-screen)
+  (print-word-slots (string-length (get-new-noun))))
 
 (hangman)
